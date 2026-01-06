86ce8dfa1d4d289f70e2e066eed4fbaa5a23a8b0
lukeisbrecht <lukeisbrecht25@gmail.com>
2026-01-05 21:02:52 -0500

    Updated for version 1.21.11

--- src/main/java/lukeisbrecht/notrampling/command/TramplingProtectionCommand.java
+++ src/main/java/lukeisbrecht/notrampling/command/TramplingProtectionCommand.java
@@ -3,23 +3,33 @@ package lukeisbrecht.notrampling.command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import lukeisbrecht.notrampling.NoCropTrampling;
 import lukeisbrecht.notrampling.config.TrampleProtection;
 import lukeisbrecht.notrampling.data.DataManager;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.permission.Permission;
+import net.minecraft.command.permission.PermissionPredicate;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class TramplingProtectionCommand {
     public static TrampleProtection protectionLevel = DataManager.getData().protection;
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(CommandManager.literal("tramplingProtection")
-                .requires(source -> source.hasPermissionLevel(2))
+                .requires(source -> {
+                    if (source.getPlayer() != null) {
+                        return source.getServer().getPlayerManager().isOperator(source.getPlayer().getPlayerConfigEntry());
+                    }
+                    return true;
+                })
+                .executes(TramplingProtectionCommand::getCurrentProtection)
                 .then(CommandManager.argument("level", StringArgumentType.word())
                         .suggests((context, builder) -> {
-                            for (TrampleProtection trampleProtection : TrampleProtection.values()) {
-                                builder.suggest(trampleProtection.name().toLowerCase());
+                            for (TrampleProtection level : TrampleProtection.values()) {
+                                builder.suggest(level.name().toLowerCase());
                             }
                             return builder.buildFuture();
                         })
@@ -44,5 +54,13 @@ public class TramplingProtectionCommand {
             context.getSource().sendError(Text.literal("Invalid protection level"));
             return 0;
         }
+    }
+
+    private static int getCurrentProtection(CommandContext<ServerCommandSource> context) {
+        TrampleProtection current = DataManager.getData().protection;
+        context.getSource().sendFeedback(
+                () -> Text.literal("Current Protection Level: " + current.getDisplayName()), false
+        );
+        return 1;
     }
 }
